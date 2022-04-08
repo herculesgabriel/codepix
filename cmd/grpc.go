4e5801269be16c806dcce2a47dfa1a7b5b08e220
Hercules Gabriel <herculesgabriel00@gmail.com>
2022-04-07 21:24:31 -0300

    chore: add cobra-cli and set up grpc command

---
+++ cmd/grpc.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/herculesgabriel/codepix/application/grpc"
+	"github.com/herculesgabriel/codepix/infrastructure/db"
+	"github.com/jinzhu/gorm"
+	"github.com/spf13/cobra"
+)
+
+var portNumber int
+
+// grpcCmd represents the grpc command
+var grpcCmd = &cobra.Command{
+	Use:   "grpc",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var database *gorm.DB = db.ConnectDB(os.Getenv("env"))
+		grpc.StartGrpcServer(database, portNumber)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(grpcCmd)
+	grpcCmd.Flags().IntVarP(&portNumber, "port", "p", 50051, "gRCP server port")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// grpcCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// grpcCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
